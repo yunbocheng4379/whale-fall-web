9462c6624b708c7f61002c8b669d3e7f71aff08a
程云博 <chengyunbo@gyyx.cn>
2025-03-02 18:48:14 +0800

    feat: :tada:测试菜单权限

--- config/routes.js
+++ config/routes.js
@@ -18,7 +18,33 @@
     icon: 'icon-home',
     access: 'allowAccessDemoRoute'
   },
-  { path: "/*", component: "@/pages/404.jsx" },
+  {
+    path: '',
+    name: '路由',
+    icon: 'VideoCameraOutlined',
+    routes: [
+      {
+        path: 'demo1',
+        name: '路由一',
+        component: '@/pages/Demo/DemoOne'
+      },
+      {
+        path: 'demo2',
+        name: '路由二',
+        component: '@/pages/Demo/DemoTwo',
+        hideInMenu: true,
+      },
+      {
+        path: 'demo3',
+        name: '路由三',
+        component: '@/pages/Demo/DemoThree',
+      }
+    ]
+  },
+  {
+    path: "/*",
+    component: "@/pages/404.jsx"
+  }
 ]
 
 export default routes
